5d33d065e134d2fa4a77093ab82079ea9c481ee3
Muxasan <mux012@yandex.ru>
2018-12-08 20:43:23 +0500

    Add files via upload

--- 1stLab.py
+++ 1stLab.py
@@ -1,4 +1,5 @@
 from pymorphy2 import MorphAnalyzer as MA
+
 from nltk.tokenize import PunktSentenceTokenizer as PST
 from nltk.tokenize import WordPunctTokenizer as WPT
 import string
@@ -21,10 +22,29 @@ text2 = """Каждый человек имеет право на образов
 а доступ к высшему образованию должен быть обеспечен наравне со всеми и
 на основе компетенции."""
 
-tokens1 = wt.tokenize(text1)
+tokens1 = wt.tokenize(text1)        # Разбиваем на слова
 tokens2 = wt.tokenize(text2)
+functors_pos = {'INTJ', 'PRCL', 'CONJ', 'PREP'}                     # Удаляем пунктуацию
 tokens1 = [i for i in tokens1 if ( i not in string.punctuation )]
 tokens2 = [i for i in tokens2 if ( i not in string.punctuation )]
-print(tokens1)
-print(tokens2)
-    
+tokens1 = [i for i in tokens1 if ( ma.parse(i)[0].tag.POS not in functors_pos )]   # Удаляем служебные части речи
+tokens2 = [i for i in tokens2 if ( ma.parse(i)[0].tag.POS not in functors_pos )]
+i = 0
+while i<len(tokens1):                     # Переводим в нижний регистр
+    tokens1[i] = tokens1[i].lower()
+    i = i + 1
+i = 0
+while i<len(tokens2):
+    tokens2[i] = tokens2[i].lower()
+    i = i + 1
+i = 0
+while i<min(len(tokens1),len(tokens2)):
+    if tokens1[i] == tokens2[i]:
+        i = i + 1
+    else:
+        
+        print(result)
+
+#print(tokens1)
+#print(tokens2)
+
